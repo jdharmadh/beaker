bc31090c7c7ec419b82fe587e644f529ff363134
Jay <jayd1903@gmail.com>
2025-07-05 18:59:57 -0700

    Initial Commit

---
+++ beaker/beakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  beakerApp.swift
+//  beaker
+//
+//  Created by Jay Dharmadhikari on 7/5/25.
+//
+
+import SwiftUI
+
+@main
+struct beakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
